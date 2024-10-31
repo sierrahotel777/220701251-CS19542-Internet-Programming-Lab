ba0750d169104fba1c02f66485f47a88f49b66d9
sierrahotel777 <78953691+sierrahotel777@users.noreply.github.com>
2024-10-31 19:15:22 +0530

    IP IEEE Project

---
+++ grocery store/about.php
@@ -0,0 +1,49 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About Us</title>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="about"><br><br><br><br><br><br><br><br><br>
+   <div class="row">
+      <div class="box">
+         <h3>why choose us?</h3>
+         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam, a quod, quis alias eius dignissimos pariatur laborum dolorem ad ullam iure, consequatur autem animi illo odit! Atque quia minima voluptatibus.</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+      <div class="box">
+         <h3>what we provide?</h3>
+         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam, a quod, quis alias eius dignissimos pariatur laborum dolorem ad ullam iure, consequatur autem animi illo odit! Atque quia minima voluptatibus.</p>
+         <a href="shop.php" class="btn">our shop</a>
+      </div>
+   </div><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</section>
+
+<?php include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ grocery store/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+
+
+   <p class="credit"> &copy; copyright @ <?= date('Y'); ?> by <span>Sanjeevan Hari</span> | all rights reserved! </p>
+
+</footer>
